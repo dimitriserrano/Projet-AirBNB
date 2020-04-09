a9c8e83fb59511fe540849d691e4ec5c8a3215dc
Corentin <corentin.beuf@ynov.com>
2020-04-09 13:17:26 +0200

    Rajout de contenu bootstrap

--- public/connexion.php
+++ public/connexion.php
@@ -7,15 +7,14 @@
         <main>
             <form>
                 <div class="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    <label for="exampleInputEmail1">Email</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Adresse mail...">
                 </div>
                 <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    <label for="exampleInputPassword1">Mot de passe</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Mot de passe...">
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-primary">Se connecter</button>
             </form>
         </main>
     </div>
--- public/membre/ajoutBien.php
+++ public/membre/ajoutBien.php
@@ -1,8 +1,33 @@
-<?php require_once '../../views/layout/header.php';
+<?php require_once '../../views/layout/header.php'; ?>
 
-//titre
-//description
-//photo
-//places dispo
-//adresse
-//prix par personne
+<div class="container">
+    <br>
+    <form>
+        <div class="form-group">
+            <label for="exampleFormControlInput1">Titre</label>
+            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Entrez le titre de votre bien...">
+        </div>
+        <div class="form-group">
+            <label for="exampleFormControlTextarea1">Description</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Entrez une description de votre bien..."></textarea>
+        </div>
+        <div class="form-group">
+            <label for="exampleFormControlInput1">Places dans le logement</label>
+            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Entrez le nombre de personne qui peuvent loger dans votre bien...">
+        </div>
+        <div class="form-group">
+            <label for="exampleFormControlInput1">Lieu</label>
+            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Entrez le lieu de votre bien...">
+        </div>
+        <div class="form-group">
+            <label for="exampleFormControlInput1">Prix par personne</label>
+            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Entrez le prix pour la réservation de votre bien...">
+        </div>
+        <div class="form-group">
+            <label for="exampleFormControlFile1">Photo du bien</label>
+            <input type="file" class="form-control-file" id="exampleFormControlFile1">
+        </div>
+    </form>
+</div>
+
+<?php require_once '../../views/layout/footer.php'; ?>
