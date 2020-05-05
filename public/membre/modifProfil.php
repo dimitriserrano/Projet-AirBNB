276419e86334d432ead8eca235590cd73bf07b00
Corentin <corentin.beuf@ynov.com>
2020-05-05 23:16:35 +0200

    Finalisation de l'html de toutes les pages + corrections de bug

--- public/membre/modifProfil.php
+++ public/membre/modifProfil.php
@@ -1,4 +1,34 @@
 <?php require_once '../../views/layout/header.php'; ?>
 
+<div class="container">
+    <br>
+    <br>
+    <form>
+        <div class="form-group">
+            <label for="exampleInputTextarea"><span class="badge badge-secondary">Titre :</span></label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputTextarea"><span class="badge badge-secondary">Localisation :</span></label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputTextarea"><span class="badge badge-secondary">Nombre de personnes :</span></label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputTextarea"><span class="badge badge-secondary">Prix par nuit :</span></label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+    </form>
+    <br>
+    <br>
+    <h3>Vos annonces :</h3>
 
-<?php require_once '../../views/layout/header.php'; ?>
+    <br>
+    <br>
+    <button type="submit" class="btn btn-primary">Sauvegarder</button>
+    <button type="submit"><a class="nav-link" href="profil.php">Quitter</button>
+</div>
+
+<?php require_once '../../views/layout/footer.php'; ?>
--- public/membre/profil.php
+++ public/membre/profil.php
@@ -1,10 +1,18 @@
 <?php require_once '../../views/layout/header.php';?>
 
+<div class="container">
+    <br>
+    <h1>Votre profil </h1>
+    <br>
+    //photo de profil
+    <p>Nom :</p>
+    <p>Prénom :</p>
+    <p>Votre adresse mail :</p>
+    <br>
+    <h3>Mes annonces :</h3><button type="submit"><a class="nav-link" href="ajoutBien.php">Ajouter une annonce</button>
+    //visualiser toute les annonces de la personne
+    <button type="submit"><a class="nav-link" href="modifProfil.php">Modifier mon annonce</button>
+    //suppression d'un bien
+</div>
 
-//prénom
-//nom
-//email
-//photo de profil
-//visualiser toute les annonces de la personne
-// bouton pour ajouter une annonce
-//bouton mofification/suppression d'un bien
+<?php require_once '../../views/layout/footer.php'; ?>
