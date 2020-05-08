7be35143b499057bb3e0984f5c954759707dea41
Corentin <corentin.beuf@ynov.com>
2020-05-08 22:26:33 +0200

    Changement de la page de bien ==> ajouter des choses dans la bdd pour voir si ça marche

---
+++ public/recherche2(test).php
@@ -0,0 +1,95 @@
+<div class="card-deck">
+    <div class="card" style="width: 18rem;">
+        <img src="./Images/appartement.jpg" class="card-img-top" alt="appartement">
+        <div class="card-body">
+            <h5 class="card-title">Appartement (+lieu)</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#"><a class="nav-link" href="bien.php"/>Voir l'annonce</a>
+        </div>
+    </div>
+    <br>
+    <br>
+    <div class="card" style="width: 18rem;">
+        <img src="./Images/appartement.jpg" class="card-img-top" alt="appartement">
+        <div class="card-body">
+            <h5 class="card-title">Appartement (+lieu)</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#"><a class="nav-link" href="bien.php"/>Voir l'annonce</a>
+        </div>
+    </div>
+    <br>
+    <br>
+    <div class="card" style="width: 18rem;">
+        <img src="./Images/appartement.jpg" class="card-img-top" alt="appartement">
+        <div class="card-body">
+            <h5 class="card-title">Appartement (+lieu)</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#"><a class="nav-link" href="bien.php"/>Voir l'annonce</a>
+        </div>
+    </div>
+</div>
+<br>
+<br>
+<div class="card-deck">
+    <div class="card" style="width: 18rem;">
+        <img src="./Images/appartement.jpg" class="card-img-top" alt="appartement">
+        <div class="card-body">
+            <h5 class="card-title">Appartement (+lieu)</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#"><a class="nav-link" href="bien.php"/>Voir l'annonce</a>
+        </div>
+    </div>
+    <br>
+    <br>
+    <div class="card" style="width: 18rem;">
+        <img src="./Images/appartement.jpg" class="card-img-top" alt="appartement">
+        <div class="card-body">
+            <h5 class="card-title">Appartement (+lieu)</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#"><a class="nav-link" href="bien.php"/>Voir l'annonce</a>
+        </div>
+    </div>
+    <br>
+    <br>
+    <div class="card" style="width: 18rem;">
+        <img src="./Images/appartement.jpg" class="card-img-top" alt="appartement">
+        <div class="card-body">
+            <h5 class="card-title">Appartement (+lieu)</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#"><a class="nav-link" href="bien.php"/>Voir l'annonce</a>
+        </div>
+    </div>
+</div>
+<br>
+<br>
+<div class="card-deck">
+    <div class="card" style="width: 18rem;">
+        <img src="./Images/appartement.jpg" class="card-img-top" alt="appartement">
+        <div class="card-body">
+            <h5 class="card-title">Appartement (+lieu)</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#"><a class="nav-link" href="bien.php"/>Voir l'annonce</a>
+        </div>
+    </div>
+    <br>
+    <br>
+    <div class="card" style="width: 18rem;">
+        <img src="./Images/appartement.jpg" class="card-img-top" alt="appartement">
+        <div class="card-body">
+            <h5 class="card-title">Appartement (+lieu)</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#"><a class="nav-link" href="bien.php"/>Voir l'annonce</a>
+        </div>
+    </div>
+    <br>
+    <br>
+    <div class="card" style="width: 18rem;">
+        <img src="./Images/appartement.jpg" class="card-img-top" alt="appartement">
+        <div class="card-body">
+            <h5 class="card-title">Appartement (+lieu)</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#"><a class="nav-link" href="bien.php"/>Voir l'annonce</a>
+        </div>
+    </div>
+</div>
+</div>
