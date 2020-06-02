87d0d1c1856af5d52fa608a26d843ff256dca619
Corentin <corentin.beuf@ynov.com>
2020-06-02 17:23:09 +0200

    Rectification fonction db.php

--- functions/db.php
+++ functions/db.php
@@ -4,12 +4,12 @@
 function getPdo(): PDO
 {
     try {
-       $pdo = new PDO (
-           "mysql:host=localhost;dbname=projetweb",
-           "airbnb1",
-           "M54RjQhx7Z8QyTnj"
-       );
-       return $pdo;
+        $pdo = new PDO (
+            "mysql:host=localhost;dbname=projetweb",
+            "projetweb",
+            "D5OZCEHLVQDJ8zCi"
+        );
+        return $pdo;
     } catch (PDOException $ex){
         exit("Erreur lors de la connexion à la base de données");
     }
